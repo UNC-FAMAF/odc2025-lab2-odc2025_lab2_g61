b88fb9a2e03b42bc236f0c5deb7af27fee2ceacb
FamafSantiagoGonzalez <santiago.gonzalez.246@mi.unc.edu.ar>
2025-06-06 22:46:30 -0300

    Cambiando fondo de figuras

--- ejercicio1/nose1.s
+++ ejercicio1/nose1.s
@@ -4,7 +4,153 @@ nose1:
   SUB    SP, SP, #16
   STP    X29, X30, [SP]     // store old FP, old LR at [SP+0] and [SP+8]
   MOV    X29, SP            // new frame pointer (not used here except to restore)
-	
+
+// background
+
+    // Rombo 1
+    movz x22, #20
+    movz x23, #34
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 2
+    movz x22, #360
+    movz x23, #120
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 3
+    movz x22, #440
+    movz x23, #13
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+
+    // Rombo 4
+    movz x22, #70
+    movz x23, #320
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 5
+    movz x22, #400
+    movz x23, #325
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 6
+    movz x22, #560
+    movz x23, #320
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 7
+    movz x22, #25
+    movz x23, #420
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 8
+    movz x22, #300
+    movz x23, #30
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 9
+    movz x22, #160
+    movz x23, #10
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 10
+    movz x22, #600
+    movz x23, #100
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+
+    // Rombo 11
+	movz x22, #5
+    movz x23, #240
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; mov x8, x23; movz x6, 60; movz x9, 30; bl rectangle
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; add x5, x22, #30; mov x8, x23; movz x6, 30; movz x9, 30; bl triangle_0
+    movz x1, 0xFF; movz x2, 0x95; movz x3, 0xA5; movz x4, 0xA6; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 60; movz x9, 30; bl rectangle
+    movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; add x5, x22, #30; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_1
+	movz x1, 0xFF; movz x2, 0xB5; movz x3, 0xCA; movz x4, 0xD5; bl build_color; mov x5, x22; add x8, x23, #30; movz x6, 30; movz x9, 30; bl triangle_0
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	movz x1, 0xFF
 	movz x2, 0xF0
 	movz x3, 0x46
@@ -182,9 +328,9 @@ nose1:
 	bl ellipse
 
 	movz x1, 0xFF
-	movz x2, 0xF0
-	movz x3, 0xF0
-	movz x4, 0xF0
+ 	movz x2, 0xB5
+	movz x3, 0xCA
+	movz x4, 0xD5
 	bl build_color
 	movz x5, 160
 	movz x8, 280
@@ -193,9 +339,9 @@ nose1:
 	bl rectangle
 
 	movz x1, 0xFF
-	movz x2, 0xF0
-	movz x3, 0xF0
-	movz x4, 0xF0
+ 	movz x2, 0xB5
+	movz x3, 0xCA
+	movz x4, 0xD5
 	bl build_color
 	movz x5, 240
 	movz x8, 280
@@ -204,9 +350,9 @@ nose1:
 	bl triangle_0
 
 	movz x1, 0xFF
-	movz x2, 0xF0
-	movz x3, 0xF0
-	movz x4, 0xF0
+	movz x2, 0xB5
+	movz x3, 0xCA
+	movz x4, 0xD5
 	bl build_color
 	movz x5, 220
 	movz x6, 40
